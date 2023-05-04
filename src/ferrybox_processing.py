54990dda8e7b473f677838a12158480fb6c04898
Niten <gusnitol@student.gu.se>
2023-05-04 16:30:59 +0200

    Add ctd station to distance plot

--- src/ferrybox_processing.py
+++ src/ferrybox_processing.py
@@ -18,6 +18,14 @@ _figsize = (14, 7)
 
 
 # %%
+ferry_file = pathlib.Path('/Volumes/Rayleigh/cruise-oc4920/data/Ferrybox/2023-05-03_09.49.44_Amerikakajen_to__741601.csv')
+ctd_positions_file = pathlib.Path(
+    '/Volumes/Rayleigh/cruise-oc4920/data/ctd_positions.tsv'
+)
+bathymetry_file = '/Volumes/Rayleigh/cruise-oc4920/meta/gebco_2022_n60.0_s54.0_w7.5_e15.0.nc'
+
+
+# %%
 temperature_colour = 'tab:orange'
 temperature_inlet_colour = 'tab:purple'
 
@@ -27,19 +35,11 @@ oxygen_colour = 'tab:red'
 
 
 # %%
-ferry_file = pathlib.Path('/Volumes/Rayleigh/cruise-oc4920/data/Ferrybox/2023-05-03_09.49.44_Amerikakajen_to__741601.csv')
-ctd_positions_file = pathlib.Path(
-    '/Volumes/Rayleigh/cruise-oc4920/data/ctd_positions.tsv'
-)
-bathymetry_file = '/Volumes/Rayleigh/cruise-oc4920/meta/gebco_2022_n60.0_s54.0_w7.5_e15.0.nc'
-
-
-# %%
-df = pandas.read_csv(ferry_file, skiprows=16, encoding='iso8859-1', sep='\t', parse_dates=['Date', 'Time'], keep_date_col=True)
+df = pandas.read_csv(ferry_file, skiprows=16, encoding='iso8859-1', sep='\t', parse_dates=[['Date', 'Time']], keep_date_col=True)
 
 data = df.to_xarray()
 data = data.assign_coords(dict(
-    time=data.Time,
+    time=data.Date_Time,
     latitude=data.Latitude, 
     longitude=data.Longitude
     )
@@ -47,8 +47,9 @@ data = data.assign_coords(dict(
 
 bathy = xarray.open_dataarray(bathymetry_file)
 bathy.name = 'bathymetry'
-# bathy = bathy.where(bathy <= 0.)
 
+
+# %%
 
 # %%
 def rot_ticks(ax, rotation=0, horizontal_alignment='center'):
@@ -94,7 +95,6 @@ def plot_wrt_x(_data, *, x_variable):
     fig, axes = pyplot.subplots(3, 1, figsize=_figsize, sharex='all')
 
     ax_temp = axes[0]
-    ax_temp_inlet = ax_temp.twinx()
 
     ax_salinity = axes[1]
     ax_chlorophyll = axes[2]
@@ -108,33 +108,25 @@ def plot_wrt_x(_data, *, x_variable):
     oxygen = _data['Oxygen']
     
     temperature.plot(x=x_variable, ax=ax_temp, color=temperature_colour)
-    # temperature_inlet.plot(x=x_variable, ax=ax_temp_inlet, c=temperature_inlet_colour)
-    # ax_temp.set_ylabel('Temp_SBE45 (˚C)', color=temperature_colour)
     ax_temp.tick_params(axis='y', labelcolor=temperature_colour)
-    # ax_temp_inlet.set_ylabel('Temp_inlet (˚C)', color=temperature_inlet_colour)
-    ax_temp_inlet.tick_params(axis='y', labelcolor=temperature_inlet_colour)
+
     
     _y_min = min(min(temperature.values), min(temperature_inlet.values))
     _y_max = max(max(temperature.values), max(temperature_inlet.values))
-    ax_temp.set_ylim((_y_min, _y_max))
-    ax_temp_inlet.set_ylim((_y_min, _y_max))
     
     salinity.plot(x=x_variable, ax=ax_salinity, color=salinity_colour)
-    # ax_salinity.set_ylabel('S (psu)', color=salinity_colour)
     ax_salinity.tick_params(axis='y', labelcolor=salinity_colour)
     
     chlorophyll.plot(x=x_variable, ax=ax_chlorophyll, color=chlorophyll_colour)
     oxygen.plot(x=x_variable, ax=ax_oxygen, color=oxygen_colour)
     ax_chlorophyll.set_ylim((0, 2.5))
     ax_oxygen.set_ylim((7, 12))
-    # ax_chlorophyll.set_ylabel('$\mu$g·l$^{-1}$', color=chlorophyll_colour)
     ax_chlorophyll.tick_params(axis='y', labelcolor=chlorophyll_colour)
-    # ax_oxygen.set_ylabel('mg·l$^{-1}$', color=oxygen_colour)
     ax_oxygen.tick_params(axis='y', labelcolor=oxygen_colour)
     
     for ax in fig.axes: ax.grid(True)
     
-    axs = [ax_temp, ax_temp_inlet, ax_salinity]
+    axs = [ax_temp, ax_salinity]
     for ax in axs: ax.set_xlabel('')
     
     if 'Time' in x_variable:
@@ -145,7 +137,6 @@ def plot_wrt_x(_data, *, x_variable):
         fig, 
         dict(
             ax_temp=ax_temp, 
-            ax_temp_inlet=ax_temp_inlet,
             ax_salinity=ax_salinity,
             ax_chlorophyll=ax_chlorophyll,
             ax_oxygen=ax_oxygen
@@ -173,7 +164,7 @@ def cumulative_distance(dataset):
 station_02_time = datetime.datetime(2023, 5, 3, 9, 29)
 timestamp_station_02 = numpy.datetime64(station_02_time)
 
-selector = dict(index=data.index[data.Time > timestamp_station_02].values)
+selector = dict(index=data.index[data.time > timestamp_station_02].values)
 _data = data.sel(selector)
 distances = cumulative_distance(_data)
 _data = _data.assign_coords(
@@ -186,8 +177,35 @@ _data = _data.assign_coords(
     )
 )
 
+# %%
+if ctd_positions_file.exists():
+    ctd_positions = pandas.read_csv(ctd_positions_file, sep='\t', parse_dates=[['date', 'time']]).to_xarray()    
+    selector = dict(
+        index=ctd_positions.index[[('CTD' in sn) for sn in ctd_positions.station_name.values]].values
+    )
+    ctd_positions = ctd_positions.sel(selector)
+
+    ctd_times = ctd_positions.date_time.values
+    ctd_station_names = ctd_positions.station_name.values
+    indices = [
+        (_data.index[_ctd_time == _data.time].values, sn)
+        for _ctd_time, sn in zip(ctd_times, ctd_station_names)
+    ]
+    indices = [(idx[0], sn) for idx, sn in indices if len(idx) > 0]
+    ctd_station_selector = dict(index=[idx[0] for idx in indices])
+    station_data = _data.sel(ctd_station_selector)
+    station_data['station_name'] = xarray.DataArray(
+        [sn for idx, sn in indices],
+        coords=dict(index=station_data.index),
+        dims=['index']
+    )
+    
+    
+# %%
 res = plot_wrt_x(data, x_variable='time')
 
+
+# %%
 fig, axes = plot_wrt_x(_data, x_variable='distance')
 ax_chlorophyll = axes['ax_chlorophyll']
 ax_oxygen = axes['ax_oxygen']
@@ -196,14 +214,14 @@ ax_chlorophyll.set_ylim((-0.2, .8))
 
 ax_oxygen.set_xlim((0, 70))
 
-# %%
-ctd_positions = pandas.read_csv(ctd_positions_file, sep='\t', parse_dates=['date', 'time']).to_xarray()
 
-selector = dict(
-    index=ctd_positions.index[[('CTD' in sn) for sn in ctd_positions.station_name.values]].values
-)
-
-ctd_positions = ctd_positions.sel(selector)
+if ctd_positions_file.exists():
+    ax_temperature = axes['ax_temp']
+    for distance, sn in zip(station_data.distance.values, station_data.station_name.values):
+        ax_temperature.annotate(sn.split('_')[-1], xy=(distance, 1.05), xycoords=('data', 'axes fraction'))
+        ax_temperature.axvline(
+            distance, color='tab:grey'
+        )
 
 
 # %%
@@ -214,3 +232,6 @@ bathy.plot.contour(x='lon', y='lat', ax=ax, levels=numpy.linspace(-500., 0, 21),
 pyplot.colorbar(img)
 ax.set_xlim((10, 11.94780225))
 ax.set_ylim((57.3, 58.5))
+
+if ctd_positions_file.exists():
+    station_data.plot.scatter(x='longitude', y='latitude', s=9, marker='+', c='tab:red')
